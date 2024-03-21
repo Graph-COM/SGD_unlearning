3e7feeff4bd7cd7d26f433f95c20a12024e0bdf3
Wang <hwang3028@panli-srv2.ece.gatech.edu>
2024-03-21 17:38:38 -0400

    clean code

--- main.sh
+++ main.sh
@@ -5,8 +5,8 @@
 #nohup python -u main_sgd.py --lam 1e-6 --dataset CIFAR10 --projection 0 --compare_baseline_nonconvergent 1 --gpu 7 >./CIFAR10_SGD_compare_baseline_nonconvergent.log 2>&1 </dev/null &
 
 # compare sequential unlearning removal
-#nohup python -u main_sgd.py --lam 1e-6 --dataset MNIST --projection 0 --sequential 1 --gpu 6 >./MNIST_SGD_sequential_32_64.log 2>&1 </dev/null &
-#nohup python -u main_sgd.py --lam 1e-6 --dataset CIFAR10 --projection 0 --sequential 1 --gpu 7 >./CIFAR10_SGD_sequential_32_64.log 2>&1 </dev/null &
+#nohup python -u main_sgd.py --lam 1e-6 --dataset MNIST --projection 0 --sequential 1 --gpu 6 >./MNIST_SGD_sequential.log 2>&1 </dev/null &
+#nohup python -u main_sgd.py --lam 1e-6 --dataset CIFAR10 --projection 0 --sequential 1 --gpu 7 >./CIFAR10_SGD_sequential.log 2>&1 </dev/null &
 
 # paint unlearning unlearning - sigma figure
 #nohup python -u main_sgd.py --lam 1e-6 --dataset MNIST --projection 0 --paint_unlearning_sigma 1 --gpu 0 >./MNIST_SGD_paint_unlearning_sigma_001.log 2>&1 </dev/null &
